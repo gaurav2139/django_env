709d7418666682ef035b8cf5142494dec411e263
Gaurav <38208095+gaurav2139@users.noreply.github.com>
2020-03-29 01:05:23 +0530

    Database and Migrations

--- djnago_projects/blog/views.py
+++ djnago_projects/blog/views.py
@@ -1,26 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Gaurav Choube',
-        'title': 'First post of the blog',
-        'content': 'Ethics and Values related to AI',
-        'date_psoted': 'March 26, 2020'
-    },
-    {
-        'author': 'Andrew Ng',
-        'title': 'Second post of the blog',
-        'content': 'Tuning Hyperparameters and regularization of models',
-        'date_psoted': 'March 26, 2020'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-            'posts': posts
+            'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html', {'title': 'About  '})
+    return render(request, 'blog/about.html', {'title': 'About  '}) 
